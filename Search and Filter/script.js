bf7d7b460f0a266f5a2fdef1995e389ae62f6533
srinukanna <kannasrinivasulu8@gmail.com>
2026-07-25 18:40:35 +0530

    version 2

--- Search and Filter/script.js
+++ Search and Filter/script.js
@@ -2,66 +2,77 @@ let products = [{
    Product: 'Mac m1 air',
    Price: 100000,
    Id:101,
+   image: "../Search and Filter/assets/images/mac  m1 air.jpg",
    Seller:'apple company',
    Instock:true
 },{
    Product: 'vivo y19',
    Price: 14000,
    Id:102,
+   image:"../Search and Filter/assets/images/vivo y19.jpeg",
    Seller:'vivo official store',
    Instock:true
 },{
    Product: 'Motorola 5g',
    Price: 15000,
    Id:103,
+   image:"../Search and Filter/assets/images/motorola 5g.jpeg",
    Seller:'vivo pvt limited',
    Instock:false
 },{
    Product: 'Oppo',
    Price: 50000,
    Id:104,
+   image:"../Search and Filter/assets/images/oppo f25.jpeg",
    Seller:'Reliance digital',
    Instock:false
 },{
    Product: 'Lenovo Laptop',
    Price: 55000,
    Id:105,
+   image:"../Search and Filter/assets/images/lenovo windows 11.jpeg",
    Seller:'Lenovo India store',
    Instock:true
 },{
    Product: 'Samsung Galaxy',
    Price: 150000,
    Id:106,
+   image:"../Search and Filter/assets/images/Samsung galaxy.jpeg",
    Seller:'Click tech retail',
    Instock:false
 },{
    Product: 'One plus nord5 5g',
    Price: 40000,
    Id:107,
+   image:"../Search and Filter/assets/images/one plus nord 5 5g.jpeg",
    Seller:'Croma store',
    Instock:true
 },{
    Product: 'Dell inspiron 15 windows 11 zlaptop',
    Price: 48000,
    Id:108,
+   image:"../Search and Filter/assets/images/dell inspiron.jpeg",
    Seller:'Croma store',
    Instock:true
 },{
    Product: 'hp 15 pavillion',
    Price: 62000,
    Id:109,
+   image:"../Search and Filter/assets/images/hp pavillion.jpeg",
    Seller:'Reliance digital',
    Instock:false
 },{
    Product: 'vivo y400 5g',
    Price: 33000,
    Id:110,
+   image:"../Search and Filter/assets/images/vivo y400 5g pro.jpeg",
    Seller:'Reliance digital',
    Instock:false
 },{
    Product: 'mac m5 pro',
    Price: 80000,
    Id:111,
+   image:"../Search and Filter/assets/images/macbook m5 pro.jpeg",
    Seller:'Reliance digital',
    Instock:true
 }];
@@ -83,10 +94,14 @@ function displayProducts(productList){
    }
    const productcards= productList.map((product)=>{
       return `<article class="product-cards ${product.Instock?"":"out_of_stock"}">
+      ${product.image ? `<img src="${product.image}" class="product_image">` : ''}
+      <div class = "content_wrapper"> 
       <h2> ${product.Product}</h2>
       <p> Price : ${product.Price}</p>
       <p> Instock :${product.Instock ?"available":"out of stock"} </p>
-      <p> seller : ${product.Seller}</p> </article>`;
+      <p> seller : ${product.Seller}</p>
+      </div> 
+      </article>`;
 
    });
    productContainer.innerHTML= productcards.join("");
