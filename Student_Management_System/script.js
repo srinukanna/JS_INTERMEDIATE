2323df55d72c7069672d52c1ba16580d6efe8d80
srinukanna <kannasrinivasulu8@gmail.com>
2026-07-28 23:59:07 +0530

    version 1

---
+++ Student_Management_System/script.js
@@ -0,0 +1,112 @@
+const students =[
+{
+id: 101,
+name : "kanna",
+age : 22,
+department:"CSE",
+cgpa : 8.6,
+gender : 'M',
+placed : false,
+},{
+id : 102,
+name : "ram",
+age : 22,
+department:"CSE",
+cgpa : 8.7,
+gender : 'M',
+placed : false,
+
+},{
+id : 103,
+name : "nagraj",
+age : 21,
+department:"ECE",
+cgpa : 8.9,
+gender : 'M',
+placed : true,
+},{
+id : 104,
+name : "sai",
+age : 23,
+department:"CSE",
+cgpa : 8.8,
+gender : 'M',
+placed : false,
+},{
+id : 105,
+name : "radhika",
+age : 23,
+department:"CSE",
+cgpa : 9.0,
+gender : 'F',
+placed : true,
+},{
+id : 106,
+name : "renuka",
+age : 22,
+department:"CSE",
+cgpa : 8.7,
+gender : 'F',
+placed : false,
+},{
+id : 107,
+name : "srinivas",
+age : 23,
+department:"ECE",
+cgpa : 8.6,
+gender : 'M',
+placed : false,
+},{
+id : 108,
+name : "tejaswini",
+age : 23,
+department:"ECE",
+cgpa : 8.7,
+gender : 'F',
+placed : true,   
+},{
+id : 109,
+name : "karthikeya",
+age : 22,
+department:"CSE",
+cgpa : 8.7,
+gender : 'M',
+placed : true,   
+},{
+id : 110,
+name : "Hasya",
+age : 22,
+department:"ECE",
+cgpa : 8.7,
+gender : 'M',
+placed : false,   
+}];
+
+const searchInput = document.querySelector('.search_input');
+const dataContainer = document.querySelector('.dataContainer');
+
+const allStudents = document.querySelector('.all_students');
+const cseStudents = document.querySelector('.cse');
+const eceStudents = document.querySelector('.ece');
+const placedStudents = document.querySelector('.placed');
+
+
+function displayStudents(){
+
+}
+
+allStudents.addEventListener('click',()=>{
+
+});
+
+cseStudents.addEventListener('click',()=>{
+
+});
+
+eceStudents.addEventListener('click',()=>{
+    
+});
+
+placedStudents.addEventListener('click',()=>{
+    
+});
