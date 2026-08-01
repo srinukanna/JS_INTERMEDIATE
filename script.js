8bd83cdf7d9b0d73ab4088d644ae50ac305a5a67
srinukanna <kannasrinivasulu8@gmail.com>
2026-08-01 21:21:59 +0530

    version 3

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-const arr =[];
-
-const input = document.querySelector('.test_input');
-const button = document.querySelector('.addButton');
+const students =[];
+ 
+// const input = document.querySelector('.test_input');
+const input = document.querySelector('.textInput');
+const addButton = document.querySelector('.addButton');
 const body = document.querySelector('body');
 
-console.log(input.value);
+const inputAge = document.getElementById('inputAge');
+const departmentName = document.getElementById('departmentName');
+const resultContainer = document.querySelector('.resultContainer');
+const studentContainer = document.querySelector('.studentContainer');
 
+const form = document.querySelector('.parentContainer');
+
+form.addEventListener('submit',(event)=>{
+   event.preventDefault();
+   if(inputAge.value === ""){return;}
+   const studentName = input.value.trim()
+   const studentAge = Number(inputAge.value);
+   const studentDepartment = departmentName.value.trim();
+
+   if( studentName === "" || studentDepartment === "" ){return;}
+   
+   const studentObject ={
+     id: Date.now(),
+     nameInput : studentName,
+     age  : studentAge,
+     departmentInput : studentDepartment
+   }
+ students.push(studentObject);
+ renderStudent(students);
+ 
+});
+
+function renderStudent(studentList){
+   resultContainer.textContent=`${studentList.length} records found`;
+  if(studentList.length === 0){return studentContainer.innerHTML=`<p> no student records found </p>`;}
+
+  const studentCard = studentList.map((student)=>{
+   return `<article class="studentCard ${student.age > 18 ? "letcheck":"giveTime"}">
+           <h2> Name :${student.nameInput}</h2>
+            <p> Age :${student.age}</p>
+            <p> Department name :${student.departmentInput}</p>
+            </article>`
+  });
+
+  studentContainer.innerHTML=studentCard.join("");
+}
+
+/*
 button.addEventListener('click',()=>{
    const newString = input.value.trim();
    console.log(newString);
@@ -14,3 +56,6 @@ button.addEventListener('click',()=>{
    console.log(arr);
    input.value = "";
 });
+*/
+
+
